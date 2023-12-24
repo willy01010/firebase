08e5a10090850098ed69fc8ecfbdcb6b9eecf19a
willy01010 <b0921217@cgu.edu.tw>
2023-12-24 17:35:10 +0800

    init

---
+++ public/init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+    apiKey: "yTkFHPO2ThtIrT6UtwvR8vavup55NahV4294oEnj",
+    authDomain: "cgu-project-autobrake.firebaseapp.com",
+    databaseURL: "https://cgu-project-autobrake-default-rtdb.firebaseio.com/",
+    projectId: "cgu-project-autobrake",
+    storageBucket: "gs://cgu-project-autobrake.appspot.com/",
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
